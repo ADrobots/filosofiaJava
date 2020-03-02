3124afd8c972aff05ca72023d492d8dcccdd851e
vox15v@mail.ru <vox15v@mail.ru>
2020-03-02 23:55:08 +0500

    abstract typeInformation

---
+++ src/typeInformations/ToyTest.java
@@ -0,0 +1,71 @@
+//Тестирование класса Class
+package typeInformations;
+
+interface HasBatteries{}
+interface Waterproof{}
+interface Shoots{}
+
+class Toy{
+    //Закомментируйте конструктор по умолчанию
+    //чтобы увидеть ошибку NoSuchMetodError из (*1*)
+
+    //Toy(){}
+    Toy(int i){}
+}
+
+//1)FancyToy наледуется от Toy и реализует интерфейсы HasBatteries, Waterproof, Shoots
+class FancyToy extends Toy implements HasBatteries, Waterproof, Shoots{
+    //fancy - маскарадный
+    FancyToy(){
+        super(1);
+    }
+}
+
+public class ToyTest {
+    //4)метод printInfo() использует getName() для получения полного имя пакета
+    //методы getSimpleName() - возвращает  имя без пакета, метод getCanonicalName - возвращает полное имя.
+    static void printInfo(Class cc){
+        //метод isInterface() сообщает, является ли объект Class интерфейсом
+        System.out.println("Имя класса "+cc.getName()+" является интерфейсом? ["+cc.isInterface()+"]");
+        System.out.println("Простое имя "+cc.getSimpleName());
+        System.out.println("Каноническое имя "+cc.getCanonicalName());
+    }
+
+    public static void main(String[] args) {
+        //2)В методе main() создается ссылка на Class
+        Class c=null;
+
+        try{
+            //3)Которая инициализируется информацией класса FancyToy с ипользованием метода forName()
+            //в блоке try
+            //внимание, должно указываться полное имя(волючающая имя пакета)
+            c=Class.forName("typeInformations.FancyToy");
+        }catch (ClassNotFoundException e){
+            System.out.println("Не удается найти FancyToy");
+            System.exit(0);
+        }
+
+        printInfo(c);
+        //5)метод Class.getInterface возвращает массив объектов Class, представляющих интерфейсы
+        for (Class face:c.getInterfaces()) {
+            printInfo(face);
+        }
+
+        //6)метод getSuperClass() предоставляет информацию о базовом объекте Class
+        Class up=c.getSuperclass();
+        Object obj=null;
+        try{
+            //необходим конктруктор по умолчанию:
+            //7)метод newInstance() класса Class используется для риализации "виртуального конструктора"
+            obj=up.newInstance();
+        }catch (InstantiationException e){
+            System.out.println("Не удалось найти экземпляр");
+            System.exit(1);
+        }catch (IllegalAccessException ee){
+            System.out.println("Отказано в доступе");
+            System.exit(1);
+        }
+
+        printInfo(obj.getClass());
+    }
+}
