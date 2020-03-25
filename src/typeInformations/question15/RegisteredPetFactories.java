a4a395056112c99fe7d0d34af9d3577ead5adcd3
ADrobots <vox15v@mail.ru>
2020-03-25 17:09:22 +0500

    question 15

---
+++ src/typeInformations/question15/RegisteredPetFactories.java
@@ -0,0 +1,9 @@
+package typeInformations.question15;
+
+public class RegisteredPetFactories {
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++){
+            System.out.println();
+        }
+    }
+}
