eb5ff08adc36883bcbf99a85f862c31d8017c2fb
vox15v@mail.ru <vox15v@mail.ru>
2020-03-26 22:51:02 +0500

    конспект 26.03.2020 дома

--- src/typeInformations/question16/RegisteredCoffeeFactories.java
+++ src/typeInformations/question16/RegisteredCoffeeFactories.java
@@ -6,7 +6,7 @@ package typeInformations.question16;
 public class RegisteredCoffeeFactories {
     public static void main(String[] args) {
         for(int i=0; i<10; i++){
-            System.out.println(Coffee16.createRandom());
+//            System.out.println(Coffee16.createRandom());
         }
     }
 }
